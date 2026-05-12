bb67a585db85736b657835d896d84ac67b80b4ee
Ibrahim <sehosseini@me.com>
2026-05-12 18:24:40 +0330

    Add global exception handling for internal server errors and invalid tokens
    - Implemented a global exception handler to return a 500 status code for internal server errors.
    - Added a specific exception handler for ValueError to return a 401 status code for invalid tokens.

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 # main.py
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from auth.router import router
 
 
 app = FastAPI()
 app.include_router(router)
+
+
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+	return JSONResponse(
+		status_code = 500,
+		content = {"detail": "Internal server error"}
+	)
+
+@app.exception_handler(ValueError)
+async def value_error_handler(request: Request, exc: Exception):
+	return JSONResponse(
+		status_code = 401,
+		content = {"detail": "Invalid Token"}
+	)
