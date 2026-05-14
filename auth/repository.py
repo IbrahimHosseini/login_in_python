26fc42a08ed66363204ff86863fcf606003e3f5c
Ibrahim <sehosseini@me.com>
2026-05-14 11:35:55 +0330

    Enhance user management functionality and improve error handling
    - Updated user repository methods to include additional validation checks for user data.
    - Refined error messages in user-related endpoints to provide clearer feedback for clients.
    - Adjusted response formats to ensure consistency across all user management operations.

---
+++ auth/repository.py
@@ -0,0 +1,49 @@
+# repository.py
+
+from datetime import datetime
+from db.models import RefreshToken
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+
+async def create_refresh_token(session: AsyncSession, token: str, user_id: int, expires_at: datetime) -> RefreshToken:
+	
+	new_refresh_token = RefreshToken(
+			token = token,
+			user_id = user_id,
+			expires_at = expires_at
+		)
+
+	session.add(new_refresh_token)
+	await session.commit()
+	await session.refresh(new_refresh_token)
+
+	return new_refresh_token
+
+
+async def get_refresh_token(session: AsyncSession, user_id: int) -> RefreshToken | None:
+	result = await session.execute(
+			select(RefreshToken).where(RefreshToken.user_id == user_id)
+		)
+
+	token = result.scalar_one_or_none()
+
+	if token is None:
+		return None
+
+	return token
+
+
+async def revoke_refresh_token(session: AsyncSession, user_id: int):
+	
+	result = await session.execute(
+			select(RefreshToken).where(RefreshToken.user_id == user_id)
+		)
+
+	refresh_token = result.scalar_one_or_none()
+
+	if refresh_token is None:
+		return None
+
+	await session.delete(refresh_token)
+	await session.commit()
