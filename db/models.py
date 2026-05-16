c3dcc04e996638295b52f6f6909c32053a30afdc
Ibrahim <sehosseini@me.com>
2026-05-16 07:38:27 +0330

    Add relationships between User and RefreshToken models
    - Introduced a relationship in the User model to associate it with RefreshToken instances, enhancing data integrity and retrieval.
    - Updated the RefreshToken model to include a back reference to the User model, facilitating easier access to user information from refresh tokens.

--- db/models.py
+++ db/models.py
@@ -4,12 +4,15 @@ from sqlalchemy import ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.orm.relationships import foreign
 from .base import Base
+from sqlalchemy.orm import relationship
 
 class User(Base):
     __tablename__ = "users"
     email: Mapped[str] = mapped_column(String(255), default="", unique=True, index=True)
     hashed_password: Mapped[str] = mapped_column(default="")
     is_active: Mapped[bool] = mapped_column(default=True)
+
+    refresh_tokens: Mapped[List["RefreshToken"]] = relationship("RefreshToken", back_populates = "user")
 
 
 class RefreshToken(Base):
@@ -18,3 +21,5 @@ class RefreshToken(Base):
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     expires_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
     revoked_at: Mapped[datetime | None] = mapped_column(default=None)
+
+    user: Mapped["User"] = relationship("User", back_populates = "refresh_tokens")
