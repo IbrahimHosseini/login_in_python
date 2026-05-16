2c209fed86eaba183093b1f9d13ab3211a104862
Ibrahim <sehosseini@me.com>
2026-05-16 13:11:53 +0330

    Refactor test_auth_login to include test_user fixture
    - Updated the test_auth_login function to accept a test_user fixture, enhancing the test's flexibility and clarity.
    - Simplified the response handling by condensing the post request into a single line for improved readability.

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,18 +1,13 @@
 # tests/test_auth.py
-
 import pytest
 
 
-async def test_auth_login(client):
+async def test_auth_login(client, test_user):
 	# given
 	data = {"email": "test@test.com", "password": "Test1234!"}
 
 	# when 
-	response = await client.post(
-			"/auth/login",
-			json = data
-		)
-
+	response = await client.post("/auth/login", json = data)
 
 	# then
 	assert response.status_code == 200
